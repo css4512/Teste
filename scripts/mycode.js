8c1b2e0d8b9c3ba09da887bb9da963a6be13c34f
css4512 <cristianedesenadasilva@gmail.com>
2014-11-01 12:25:51 -0300

    Teste
    teste

---
+++ scripts/mycode.js
@@ -0,0 +1,27 @@
+﻿///// <reference path="mycode.js" />
+var app = angular.module('HackingCodeApp', []);
+
+
+app.controller('MainController', function ($rootScope, $http, $scope) {
+        
+    $scope.nome = "xico";
+
+    $scope.registro = function () {
+        alert('xico');
+        $http.post('http://localhost:9090/Empresa').success(RegistroCallback);
+    }
+
+    $scope.Clear = function () {
+        
+        $scope.Participante.fone = "";
+        $scope.Participante.email = "";
+        $scope.Participante.area = "";
+    }
+
+    function RegistroCallback(data, status) {
+        $scope.objeto = data[0];
+        alert($scope.objeto);
+    }
+
+    
+});
